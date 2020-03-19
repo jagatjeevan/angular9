678b25feb7d4fe0092cf8c266da783441ca917e6
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2020-03-19 09:59:39 +0530

    fix path issue in image

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,11 +6,11 @@ import { Component } from "@angular/core";
     <h1>Here goes the dashboard</h1>
     <div class="dashboard-list">
       <div class="image-container">
-        <img src="/assets/shoppingcart.svg" class="shopping-cart" />
+        <img src="assets/shoppingcart.svg" class="shopping-cart" />
         <label><a routerLink="/orders">Orders</a></label>
       </div>
       <div class="image-container">
-        <img src="/assets/customer-list.png" class="customer-list" />
+        <img src="assets/customer-list.png" class="customer-list" />
         <label><a routerLink="/customers">Customer list</a></label>
       </div>
     </div>
